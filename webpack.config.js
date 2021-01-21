e9b21601b02325217edce12940dd5558c691d9fe
Paulo-ss <76018697+Paulo-ss@users.noreply.github.com>
2021-01-20 21:01:38 -0300

    webpack.config.js deletado

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: ['@babel/polyfill', 'whatwg-fetch', './js/script.js'],
-  output: {
-    path: path.resolve(__dirname, './main/'),
-    filename: 'main.bundle.js',
-  },
-};
