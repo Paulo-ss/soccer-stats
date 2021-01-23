3f5fcb79fce8921af4fe328c9cb09d9f06b53d4e
Paulo Silveira <paulossnjf@gmail.com>
2021-01-23 19:04:42 -0300

    Animacao ao scroll finalizada

---
+++ js/modules/scrollAnimation.js
@@ -0,0 +1,53 @@
+// Importando a função helper de debounce
+import debounce from '../helpers/debounce.js';
+
+export default class ScrollAnimation {
+  constructor(sections, active) {
+    // Selecionando as seções do site que serão animadas
+    this.sections = [...document.querySelectorAll(sections)];
+    // Valor da métade da tela do browser
+    this.halfWindow = window.innerHeight * 0.6;
+    // Classe CSS para ativar as animações das sections
+    this.active = active;
+
+    // Bind nos métodos de callback
+    this.animateScroll = debounce(this.animateScroll.bind(this), 100);
+  }
+
+  // Método que pega a distância do topo de cada section
+  getSectionsOffsetTop() {
+    this.sectionsOffsetTop = this.sections.map((section) => {
+      return {
+        element: section,
+        offsetTop: section.offsetTop - this.halfWindow,
+      };
+    });
+  }
+
+  // Método que anima as sections ao scroll
+  animateScroll() {
+    const scrollPosition = window.pageYOffset;
+
+    this.sectionsOffsetTop.forEach((i) => {
+      if (scrollPosition > i.offsetTop) {
+        i.element.classList.add(this.active);
+      }
+    });
+  }
+
+  // Método que adiciona o event de scroll ao window
+  addEvents() {
+    window.addEventListener('scroll', this.animateScroll);
+  }
+
+  // Método que inicia a classe
+  init() {
+    if (this.sections.length) {
+      this.addEvents();
+      this.getSectionsOffsetTop();
+      this.animateScroll();
+    }
+
+    return this;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ import TabNav from './modules/tabNav.js';
 import Slide from './modules/slide.js';
 // Importando a classe do menuMobile
 import MenuMobile from './modules/menuMobile.js';
+// Importando a classe que anima as seções do site ao scroll
+import ScrollAnimation from './modules/scrollAnimation.js';
 
 // Iniciando o objeto dropdown
 const dropdown = new Dropdown(
@@ -29,10 +31,17 @@ const slide = new Slide(
 );
 slide.init();
 
-// Iniciando o objeto menuMobile
+// Iniciando o objeto MenuMobile
 const menuMobile = new MenuMobile(
   '[data-mobile-toggle]',
   '[data-mobile-menu]',
   'active'
 );
 menuMobile.init();
+
+// Iniciando o objeto ScrollAnimation
+const scrollAnimation = new ScrollAnimation(
+  '[data-animate-scroll]',
+  'animate-section'
+);
+scrollAnimation.init();
