6b4c7762e030b5b7e905b8ff46dce3b6031498f7
Paulo Silveira <paulossnjf@gmail.com>
2021-01-24 14:32:49 -0300

    Classe de fetchAPI e toda sua estrutura finalizada

---
+++ js/helpers/fetchAPI.js
@@ -0,0 +1,81 @@
+export default class FetchAPI {
+  constructor(
+    appendTarget,
+    templateHTMLFunc,
+    fetchConfig,
+    classe,
+    ...classeArgs
+  ) {
+    this.appendTarget = document.querySelector(appendTarget);
+    this.templateHTMLFunc = templateHTMLFunc;
+    this.fetchConfig = fetchConfig;
+
+    // Propriedade com a classe que deverá ser criada e iniciada
+    // somente após o fetch e o novo elemento ser criado e posicionado
+    // no DOM para que a classe consiga pegá-lo
+    this.classe = classe;
+    // Argumentos que a classe a ser executada recebe
+    this.classeArgs = classeArgs;
+
+    // Estado de loading
+    this.loading = true;
+  }
+
+  // Método que cria um novo elemento com o conteúdo
+  // retornado pela API
+  createElement(data) {
+    const newElement = document.createElement('div');
+
+    newElement.innerHTML = this.templateHTMLFunc(data);
+
+    return newElement;
+  }
+
+  // Método que lida com respostas do tipo Array
+  handleArray(json) {
+    json.forEach((i) => {
+      this.appendTarget.append(this.createElement(i));
+    });
+  }
+
+  // Método que verifica o tipo de dado retornado no fetch
+  verifyData(json) {
+    return Object.prototype.toString.call(json);
+  }
+
+  // Método que dependendo do tipo de dado retornado,
+  // executa uma função ou outra
+  typeofData(json) {
+    const verifyObject = this.verifyData.bind(null);
+
+    if (verifyObject(json) === '[object Array]') {
+      this.handleArray(json);
+    }
+  }
+
+  // Método que faz o fetch na url
+  async fetchAPI() {
+    const { url, options } = this.fetchConfig();
+
+    this.loading = true;
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+
+    this.loading = false;
+    this.typeofData(json);
+
+    // Declarando a classe passando seus argumentos e a iniciando
+    const classe = new this.classe(...this.classeArgs);
+    classe.init();
+  }
+
+  // Método que inicia a classe
+  init() {
+    if (this.appendTarget) {
+      this.fetchAPI();
+    }
+
+    return this;
+  }
+}
