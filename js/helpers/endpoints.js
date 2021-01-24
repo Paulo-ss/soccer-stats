7b328ad2cb73a83ef04edd8896f877670dbc6ec7
Paulo Silveira <paulossnjf@gmail.com>
2021-01-24 18:24:36 -0300

    Fetch do slide finalizado

--- js/helpers/endpoints.js
+++ js/helpers/endpoints.js
@@ -1,10 +1,23 @@
 // Key para autorização na API
-export const apiKey = 'test_d6757ec2d3414c80aacddd7370e681';
+const apiKey = 'test_d6757ec2d3414c80aacddd7370e681';
 
 // Endpoint dos campeonatos
 export function GET_CAMPEONATOS() {
   return {
     url: 'https://api.api-futebol.com.br/v1/campeonatos',
+    options: {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${apiKey}`,
+      },
+    },
+  };
+}
+
+// Endpoint partidas ao vivo
+export function GET_PARTIDAS_AO_VIVO() {
+  return {
+    url: 'https://api.api-futebol.com.br/v1/ao-vivo',
     options: {
       method: 'GET',
       headers: {
---
+++ js/helpers/templateHTML/slideHTML.js
@@ -0,0 +1,26 @@
+// Função que retorna o template de HTML do slide
+export default function slideHTML(partida) {
+  const { placar, status, data_realizacao, hora_realizacao } = partida;
+  const { nome: nomeCampeonato } = partida.campeonato;
+  const { nome_popular: nomeEstadio } = partida.estadio;
+  const { nome_popular: nomeTimeM, escudo: escudoM } = partida.time_mandante;
+  const { nome_popular: nomeTimeV, escudo: escudoV } = partida.time_visitante;
+
+  const innerHTML = `<div class="partida" data-slide>
+                      <a href="#">
+                        <h3> ${nomeCampeonato} <span class="em-andamento"> ${status} </span> </h3>
+                        <div class="placar">
+                          <img class="escudo" src="${escudoM}" alt="Escudo do ${nomeTimeM}">
+                          <p> ${placar} </p>
+                          <img class="escudo" src="${escudoV}" alt="Escudo do ${nomeTimeV}">
+                        </div>
+                        <div class="info">
+                          <p> ${data_realizacao} </p>
+                          <p> ${hora_realizacao} </p>
+                          <p> ${nomeEstadio} </p>
+                        </div>
+                      </a>
+                    </div>`;
+
+  return innerHTML;
+}
