1a7e0cdea04c1f1e8d2bdfa60db834dbc22065c5
Paulo Silveira <paulossnjf@gmail.com>
2021-01-27 20:51:20 -0300

    Fetch de cada fase específica do campeonato finalizada

--- js/helpers/templateHTML/campFasesHTML.js
+++ js/helpers/templateHTML/campFasesHTML.js
@@ -1,18 +1,18 @@
 // Função que retorna o innerHTML das fases do campeonato
 export default function campFasesHTML(fases) {
-  const { nome, status, tipo, fase_id } = fases;
+  const { nome, status, tipo, fase_id: faseId } = fases;
 
   // Formatando a string de tipo
   const tipoFormatado = tipo
     .split('-')
-    .map((tipo) => {
+    .map((i) => {
       const firstLetter = tipo.charAt(0).toUpperCase();
-      return tipo.replace(/\w/i, firstLetter);
+      return i.replace(/\w/i, firstLetter);
     })
     .join(' ');
 
-  const innerHTML = `<a href="#camp-fase" data-scroll-trigger>
-                      <div class="fase" id="${fase_id}">
+  const innerHTML = `<a href="#camp-fase" id="${faseId}" data-scroll-trigger data-fetch-fase>
+                      <div class="fase">
                         <div class="fase-info">
                           <h2> ${nome} </h2>
                           <p class="status"> ${status} </p>
